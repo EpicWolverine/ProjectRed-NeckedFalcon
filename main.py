0d066b7de3c8c626cc508ca890760f6b6444773e
EpicWolverine <epicwolverine@me.com>
2014-10-06 22:27:47 -0400

    Wrote all data outputs needed for the project

--- main.py
+++ main.py
@@ -18,18 +18,59 @@
 
 """<insert man page here>"""
 
-import steamapi
+import steamapi     # Smiley Barry's library for accessing the Steam Web API
+import sys          # for various I/O functions
+import time         # datetime support
 
 myapikey=open("apikey.txt").read() #change this to retrieve api key from a text file later
 
-def main():
+def main(SteamUserID):
+    # Timestamp
+    print "Date Accessed: " + time.strftime("%Y/%m/%d %H:%M:%S")
+    
+    # Initialize user
     steamapi.core.APIConnection(api_key=myapikey)
-    currentuser = steamapi.user.SteamUser()
-    print currentuser.recently_played
-    print currentuser.recently_played[0].playtime_2weeks
-    print currentuser.recently_played[0].playtime_forever
+    currentuser = steamapi.user.SteamUser(SteamUserID)
+    
+    # User's Profile Name
+    print "Profile Name: " + currentuser.name
+    
+    # Note to Self
+    print "Note: All playtimes are in minutes."
+    
+    # Total Play Time Across All Games
+    totalplaytime = 0
+    for game in currentuser.games:
+        totalplaytime += game.playtime_forever
+    print "Total Play Time Across All Games: " + str(totalplaytime)
+    
+    # Total Games Owned
+    print "Total Games Owned: " + str(len(currentuser.games))
+    
+    # Total Games Owned That Have Been Played for > 30 Minutes
+    playedgames = 0
+    for game in currentuser.games:
+        if game.playtime_forever > 30:
+            playedgames += 1
+    print "Total Games Played (for > 30 minutes): " + str(playedgames)
+    
+    # Recently Played
+    print "Recently Played (last 2 weeks): "
+    #print currentuser.recently_played
+    for game in currentuser.recently_played:
+        print "- " + str(game)
+        print "  2 weeks: " + str(game.playtime_2weeks)
+        print "  Forever: " + str(game.playtime_forever)
+    #print currentuser.recently_played[0].playtime_2weeks
+    #print currentuser.recently_played[0].playtime_forever
+    
+    # Account Active?
+    useractive = False
+    if len(currentuser.recently_played) > 0:
+        useractive = True
+    print "Active?: " + str(useractive)
 	
 	
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
